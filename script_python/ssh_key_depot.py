c5915fcfda8a2400e49d93122e841dceeeb96bd6
amonzon <antoine.monzon@gmail.com>
2021-04-23 15:21:23 +0200

    ssh_key_depot.py: update fonction def, return key

--- script_python/ssh_key_depot.py
+++ script_python/ssh_key_depot.py
@@ -1,5 +1,8 @@
+file_key = "/home/administrateur/.ssh/id_rsa.pub"
+
 #recupérer la clef ssh local
-def recup_local_key(file_key)
+def recup_local_key(file_key):
   file = open(file_key, "r")
   key = file.read() 
   file.close()
+  return key
